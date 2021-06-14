bd8fa6074408ad84097c763ea57868193db37497
widi02 <widi@dev.netkromsolution.com>
2021-06-14 11:58:43 +0700

    delete

--- transmedikakitui/src/main/java/com/transmedika/transmedikakitui/widget/dynamicview/BaseErrorListener.java
+++
@@ -1,8 +0,0 @@
-package com.transmedika.transmedikakitui.widget.dynamicview;
-
-public interface BaseErrorListener {
-    void showError(String message);
-    void validationError(String message);
-    void hideError();
-    void hideValidationError();
-}
